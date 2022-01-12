845c484ec346ed9586df55d8e428305aea393191
cavenderbi <cavenderbi@appstate.edu>
2022-01-12 17:42:47 -0500

    Sprite renders to the center of the screen.

--- game.asm
+++ game.asm
@@ -9,7 +9,11 @@
 ; Public variables in this module
 ;--------------------------------------------------------
 	.globl _main
-	.globl _puts
+	.globl _draw
+	.globl _logic
+	.globl _input
+	.globl _set_sprite_data
+	.globl _heart
 ;--------------------------------------------------------
 ; special function registers
 ;--------------------------------------------------------
@@ -21,6 +25,8 @@
 ; ram data
 ;--------------------------------------------------------
 	.area _INITIALIZED
+_heart::
+	.ds 16
 ;--------------------------------------------------------
 ; absolute external ram data
 ;--------------------------------------------------------
@@ -41,21 +47,73 @@
 ; code
 ;--------------------------------------------------------
 	.area _CODE
-;game.c:4: void main() {
+;game.c:15: void input() {
+;	---------------------------------
+; Function input
+; ---------------------------------
+_input::
+;game.c:18: }
+	ret
+;game.c:20: void logic() {
+;	---------------------------------
+; Function logic
+; ---------------------------------
+_logic::
+;game.c:23: }
+	ret
+;game.c:25: void draw() {
+;	---------------------------------
+; Function draw
+; ---------------------------------
+_draw::
+;game.c:29: set_sprite_data(0, 1, heart);
+	ld	de, #_heart
+	push	de
+	xor	a, a
+	inc	a
+	push	af
+	call	_set_sprite_data
+	add	sp, #4
+;/home/brandanc/Downloads/gbdk/include/gb/gb.h:1174: shadow_OAM[nb].tile=tile;
+	ld	hl, #(_shadow_OAM + 2)
+	ld	(hl), #0x00
+;/home/brandanc/Downloads/gbdk/include/gb/gb.h:1247: OAM_item_t * itm = &shadow_OAM[nb];
+	ld	hl, #_shadow_OAM
+;/home/brandanc/Downloads/gbdk/include/gb/gb.h:1248: itm->y=y, itm->x=x;
+	ld	a, #0x48
+	ld	(hl+), a
+	ld	(hl), #0x50
+;game.c:35: SHOW_SPRITES;
+	ldh	a, (_LCDC_REG + 0)
+	or	a, #0x02
+	ldh	(_LCDC_REG + 0), a
+;game.c:36: }
+	ret
+;game.c:38: void main() {
 ;	---------------------------------
 ; Function main
 ; ---------------------------------
 _main::
-;game.c:5: printf("Hello, GameBoy!\n");
-	ld	de, #___str_1
-	push	de
-	call	_puts
-	pop	hl
-;game.c:6: }
-	ret
-___str_1:
-	.ascii "Hello, GameBoy!"
-	.db 0x00
+;game.c:39: draw();
+;game.c:40: }
+	jp	_draw
 	.area _CODE
 	.area _INITIALIZER
+__xinit__heart:
+	.db #0x66	; 102	'f'
+	.db #0x66	; 102	'f'
+	.db #0x99	; 153
+	.db #0xff	; 255
+	.db #0x81	; 129
+	.db #0xff	; 255
+	.db #0x81	; 129
+	.db #0xff	; 255
+	.db #0x42	; 66	'B'
+	.db #0x7e	; 126
+	.db #0x24	; 36
+	.db #0x3c	; 60
+	.db #0x18	; 24
+	.db #0x18	; 24
+	.db #0x00	; 0
+	.db #0x00	; 0
 	.area _CABS (ABS)
