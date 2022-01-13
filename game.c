d3927a6f8ddd0c61356cd007b809698e88b14901
cavenderbi <cavenderbi@appstate.edu>
2022-01-12 21:33:37 -0500

    Flashing square animation.

--- game.c
+++ game.c
@@ -5,36 +5,23 @@
 #include <stdio.h>
 // Include the sprites defined in their respective C folders. 
 // Thanks to GBTD for letting me not have to write 8x8 sprites in hex by hand.
-//#include "heart.h"
-extern unsigned char heart[] =
-{
-  0x66,0x66,0x99,0xFF,0x81,0xFF,0x81,0xFF,
-  0x42,0x7E,0x24,0x3C,0x18,0x18,0x00,0x00
-};
-// Function that handles the input of the game.
-void input() {
-    // Does nothing atm...
-    ;
-}
-// Function that handles the logic of the game after the input is read. 
-void logic() {
-    //Does nothing atm...
-    ;
-}
-// Function that draws the current game state to the screen. 
-void draw() {
-    // Draw a sprite in the middle of the screen.
-
-    // Load the heart sprite into memory. 
-    set_sprite_data(0, 1, heart);
-    set_sprite_tile(0, 0);
-
-    // Move the sprite to the center of the screen. 
-    move_sprite(0, SCREENWIDTH/2+4, SCREENHEIGHT/2+4);
-
-    SHOW_SPRITES;
-}
+#include "heart.c"
+#include "square.c"
 
 void main() {
-    draw();
+    uint8_t current_sprite = 0;
+
+    set_sprite_data(0, 2, square);
+    set_sprite_tile(0, current_sprite);
+
+    move_sprite(0, 88, 78);
+
+    SHOW_SPRITES;
+
+    while (true) {
+        current_sprite = !current_sprite;
+        set_sprite_tile(0, current_sprite);
+        delay(500);
+    }
+    
 }
