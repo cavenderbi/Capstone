a7546f4c371ba1e007d6d14135ac19df161fd3f2
cavenderbi <cavenderbi@appstate.edu>
2022-02-13 14:57:31 -0500

    Added multiple projectiles, though no limit on rate of fire and not properly hiding unused sprites.

--- source/projectile.c
+++ source/projectile.c
@@ -1,69 +1,65 @@
 #include "projectile.h"
 
+/*  Initialize the projectile vector. */
+void initProjs() {
+    VECTOR_INIT(proj_vector);
+}
+
 /*  Spawns a new projectile at the given x and y coordinates, facing the given direction. 
     @param x The starting x-coordinate of the projectile. 
     @param y The starting y-coordinate of the projectile. 
     @param dir The direction the projectile is facing. */
 void shoot(uint8_t x, uint8_t y, Direction dir) {
-    /*  Only allow 16 projectiles at a time, at least for now. */
-    const int MAX_PROJS = 16;
-    if (new_index <= MAX_PROJS) {
-        /*  Create the new projectile struct. */
-        Projectile * new_proj = (Projectile *) malloc(sizeof(Projectile));
-        new_proj->dir = dir;
+    /*  Limit the number of projectiles, at least for now. */
+    const int max_projs = 16;
+    if (VECTOR_SIZE(proj_vector) < max_projs) {
+        Projectile * new_proj = malloc(sizeof(Projectile));
         new_proj->x = x;
         new_proj->y = y;
-        new_proj->tile_index = 1;
-        /*  Insert the new projectile into the list. */
-        insert(proj_head, new_proj);
-        /*  Increment new index for the next projectile. */
-        new_index++;
+        new_proj->dir = dir;
+        VECTOR_ADD(proj_vector, new_proj);
     }
 }
 
-/*  Updates the projectiles position and detects projectile collision. */
-void update_projs() {
-    /*  Iterate through the list of projectiles. */
-    Node * current = proj_head;
-    while (current != NULL) {
-        /*  Update projectile position. */
-
-        /*  Take the void pointer of current node's data and cast it to a 
-            Projectile pointer. Then get that Projectile's Direction. If a 
-            non-Projectile was added to the list, this means nothing and you'll 
-            be reading completely meaningless data. */
-        Projectile * proj = (Projectile *) current->data;
-        switch (proj->dir) {
+/*  Updates the projectiles position and detects projectile collision. 
+    Then updates the relevant sprites. */
+void updateProjs() {
+    /*  Loop through all of the projectiles in the vector. */
+    int i;
+    for (i = 0; i < VECTOR_SIZE(proj_vector); i++) {
+        Projectile * current = VECTOR_GET(proj_vector, Projectile *, i);
+        switch (current->dir) {
             case UP:
-                if (!sprite_tile_collision(proj->x, proj->y - 4, 3, 8))
-                    proj->y -= 4;
-                else {
-
-                }
+                if (sprite_tile_collision(current->x, current->y - 4, current->dir)) {
+                    current->y -= 4;
+                    set_sprite_tile(1 + i, 9);
+                    set_sprite_prop(1 + i, get_sprite_prop(1 + i) | S_FLIPY);
+                    move_sprite(1 + i, current->x, current->y);
+                } else VECTOR_REMOVE(proj_vector, i--);
                 break;
             case DOWN:
-                if (!sprite_tile_collision(proj->x, proj->y + 4, 3, 8))
-                    proj->y += 4;
-                else {
-
-                }
+                if (sprite_tile_collision(current->x, current->y + 4, current->dir)) {
+                    current->y += 4;
+                    set_sprite_tile(1 + i, 9);
+                    set_sprite_prop(1 + i, get_sprite_prop(1 + i) & ~S_FLIPY);
+                    move_sprite(1 + i, current->x, current->y);
+                } else VECTOR_REMOVE(proj_vector, i--);
                 break;
             case LEFT:
-                if (!sprite_tile_collision(proj->x - 4, proj->y, 3, 8))
-                    proj->x -= 4;
-                else {
-
-                }
+                if (sprite_tile_collision(current->x - 4, current->y, current->dir)) { 
+                    current->x -= 4;
+                    set_sprite_tile(1 + i, 8);
+                    set_sprite_prop(1 + i, get_sprite_prop(1 + i) | S_FLIPX);
+                    move_sprite(1 + i, current->x, current->y);
+                } else VECTOR_REMOVE(proj_vector, i--);
                 break;
             case RIGHT:
-                if (!sprite_tile_collision(proj->x + 4, proj->y, 3, 8))
-                    proj->x += 4;
-                else {
-
-                }
-                break;
+                if (sprite_tile_collision(current->x + 4, current->y, current->dir)) {
+                    current->x += 4;
+                    set_sprite_tile(1 + i, 8);
+                    set_sprite_prop(1 + i, get_sprite_prop(1 + i) & ~S_FLIPX);
+                    move_sprite(1 + i, current->x, current->y);
+                } else VECTOR_REMOVE(proj_vector, i--);
         }
-        /*  Update current so there's not an infinite loop. */
-        current = current->next;
     }
 }
