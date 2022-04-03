20fc88c125a7a246feffad3a8a06d0fe0b7d2a26
shermanam <shermanam@appstate.edu>
2022-04-03 18:01:51 -0400

    please work

--- res/knight_walk_up.h
+++ res/knight_walk_up.h
@@ -9,7 +9,7 @@
 #define knight_walk_up_TILE_H 16
 #define knight_walk_up_WIDTH 16
 #define knight_walk_up_HEIGHT 32
-#define knight_walk_up_TILE_COUNT 4
+#define knight_walk_up_TILE_COUNT 2
 #define knight_walk_up_PIVOT_X 8
 #define knight_walk_up_PIVOT_Y 16
 #define knight_walk_up_PIVOT_W 16
@@ -18,7 +18,7 @@
 BANKREF_EXTERN(knight_walk_up)
 
 extern const palette_color_t knight_walk_up_palettes[4];
-extern const uint8_t knight_walk_up_tiles[128];
+extern const uint8_t knight_walk_up_tiles[64];
 
 extern const metasprite_t* const knight_walk_up_metasprites[1];
 
