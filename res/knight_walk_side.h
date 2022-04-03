d2e28c74132fe705fcd760f3bb72f1fe9a85639c
shermanam <shermanam@appstate.edu>
2022-04-03 17:23:11 -0400

    The barbarian isn'tm working and im

--- res/knight_walk_side.h
+++ res/knight_walk_side.h
@@ -6,21 +6,20 @@
 #include <gbdk/platform.h>
 #include <gbdk/metasprites.h>
 
-#define knight_walk_side_TILE_ORIGIN 0
-#define knight_walk_side_TILE_H 8
+#define knight_walk_side_TILE_H 16
 #define knight_walk_side_WIDTH 16
-#define knight_walk_side_HEIGHT 16
-#define knight_walk_side_TILE_COUNT 6
+#define knight_walk_side_HEIGHT 32
+#define knight_walk_side_TILE_COUNT 4
 #define knight_walk_side_PIVOT_X 8
-#define knight_walk_side_PIVOT_Y 8
+#define knight_walk_side_PIVOT_Y 16
 #define knight_walk_side_PIVOT_W 16
-#define knight_walk_side_PIVOT_H 16
+#define knight_walk_side_PIVOT_H 32
 
 BANKREF_EXTERN(knight_walk_side)
 
 extern const palette_color_t knight_walk_side_palettes[4];
-extern const uint8_t knight_walk_side_tiles[96];
+extern const uint8_t knight_walk_side_tiles[128];
 
-extern const metasprite_t* const knight_walk_side_metasprites[2];
+extern const metasprite_t* const knight_walk_side_metasprites[1];
 
 #endif
--- res/knight_walk_up.h
+++ res/knight_walk_up.h
@@ -6,21 +6,20 @@
 #include <gbdk/platform.h>
 #include <gbdk/metasprites.h>
 
-#define knight_walk_up_TILE_ORIGIN 0
-#define knight_walk_up_TILE_H 8
+#define knight_walk_up_TILE_H 16
 #define knight_walk_up_WIDTH 16
-#define knight_walk_up_HEIGHT 16
-#define knight_walk_up_TILE_COUNT 7
+#define knight_walk_up_HEIGHT 32
+#define knight_walk_up_TILE_COUNT 4
 #define knight_walk_up_PIVOT_X 8
-#define knight_walk_up_PIVOT_Y 8
+#define knight_walk_up_PIVOT_Y 16
 #define knight_walk_up_PIVOT_W 16
-#define knight_walk_up_PIVOT_H 16
+#define knight_walk_up_PIVOT_H 32
 
 BANKREF_EXTERN(knight_walk_up)
 
 extern const palette_color_t knight_walk_up_palettes[4];
-extern const uint8_t knight_walk_up_tiles[112];
+extern const uint8_t knight_walk_up_tiles[128];
 
-extern const metasprite_t* const knight_walk_up_metasprites[2];
+extern const metasprite_t* const knight_walk_up_metasprites[1];
 
 #endif
