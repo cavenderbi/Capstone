43586c40bff5ceff1b282dd2b118e88bebdb6477
cavenderbi <cavenderbi@appstate.edu>
2022-04-08 09:46:17 -0400

    Push.

--- src/camera.c
+++ src/camera.c
@@ -1,150 +1,142 @@
 #include "camera.h"
-
-Cam camera, old_camera;
-
-Map map, old_map;
+#include "rooms.h"
 
 const uint8_t * tilemap;
-uint8_t tilemap_height;
-uint8_t tilemap_width;
 uint8_t tile_offset;
+uint8_t x, y;
 
-/*  Initializes the camera scrolling behavior.  
-    @param tiles Pointer to the tiles.
-    @param tile_offset Which index do the tiles start at in VRAM? 
-    @param num_tiles The number of tiles to load into VRAM. 
-    @param tilemap Pointer to the big tilemap. 
-    @param tilemap_width Width in tiles of the tilemap.
-    @param tilemap_height Height in tiles of the tilemap. */
-void init_camera(uint8_t * tiles, uint8_t _tile_offset, uint8_t num_tiles, uint8_t * _tilemap, uint8_t _tilemap_width, uint8_t _tilemap_height) {
-    DISPLAY_OFF;
-    SHOW_BKG;
-
-    tilemap = _tilemap;
-    tilemap_height = _tilemap_height;
-    tilemap_width = _tilemap_width;
-    tile_offset = _tile_offset;
-
-    map.x_pos = 0;
-    map.y_pos = 0;
-    old_map.x_pos = 0xff;
-    old_map.y_pos = 0xff;
-
-    set_bkg_data(tile_offset, num_tiles, tiles);
-    /*  Draw a 20x18 (Gameboy screen sized) submap of the tilemap. */
-    set_bkg_based_submap(map.x_pos, map.y_pos, 20, 16, tilemap, tilemap_width, tile_offset);
-    fill_rect(0, 16, 20, 2, 0);
-    DISPLAY_ON;
-
-    camera.x_pos = 0;
-    camera.y_pos = 0;
-    old_camera.x_pos = 0;
-    old_camera.y_pos = 0;
-
-    SCX_REG = camera.x_pos;
-    SCY_REG = camera.y_pos;
+/*  Loads in a single column of a tileset.
+ *  
+ *  @param x Starting X-coordinate (in tiles).
+ *  @param y Starting Y-coordinate (in tiles).
+ *  @param row_height Number of rows in the tilemap.
+ *  @param map_width Width of the tilemap.
+ *  @param p_src_map Pointer to the tilemap to be scrolled to. 
+ */
+void set_bkg_tiles_col(uint8_t x, uint8_t y, uint8_t row_height, uint8_t map_width, const uint8_t * p_src_map) {
+    // Cache returned address of first map tile vram address
+    uint8_t * p_col_screen = (uint8_t *)set_bkg_tile_xy(x, y, *p_src_map);
+    while (--row_height) {
+        // Move to next Screen Map_y and handle screen wraparound 0x9C00 -> 9800
+        p_col_screen = (uint8_t *)((uint16_t)(p_col_screen + 32) & 0x9BFFU);
+        p_src_map += map_width; // Move to next Map_y
+        set_vram_byte(p_col_screen, *p_src_map);
+    }
 }
 
-/*  Scrolls the camera in the given direction for the given distance in tiles. 
-    @param dir Direction to scroll the camera. 
-    @param dist Distance in 8x8 tiles to scroll the camera. */
+/*  Scrolls upward to the next tilemap.
+ *
+ *  @param x X-coordinate to start at.
+ *  @param y Y-coordinate to start at.
+ *  @param tilemap Tilemap to scroll to.
+ *  @param tilemap_width Width of tilemap.
+ *  @param tilemap_height Height of tilemap.
+ *  @returns Y-coordinate ended at. 
+ */
+int scroll_up(uint8_t x, uint8_t y, const uint8_t * tilemap, uint8_t tilemap_width, uint8_t tilemap_height) {
+    int i = tilemap_height;
+    x %= 32;
+    tilemap += tilemap_width * (tilemap_height - 1);
+    while (i--) {
+        wait_vbl_done();
+        y--; y %= 32;
+        set_bkg_tiles(x, y, tilemap_width, 1u, tilemap);
+        tilemap -= tilemap_width;
+        SCY_REG -= 8;
+        delay(25);
+    }
+    return y;
+}
+
+/*  Scrolls downward to the next tilemap.
+ *
+ *  @param x X-coordinate to start at.
+ *  @param y Y-coordinate to start at.
+ *  @param tilemap Tilemap to scroll to.
+ *  @param tilemap_width Width of tilemap.
+ *  @param tilemap_height Height of tilemap.
+ *  @returns Y-coordinate ended at. 
+ */
+int scroll_down(uint8_t x, uint8_t y, const uint8_t * tilemap, uint8_t tilemap_width, uint8_t tilemap_height) {
+    int i = tilemap_height;
+    y += tilemap_height;
+    x %= 32;
+    while (i--) {
+        wait_vbl_done();
+        set_bkg_tiles(x, y, tilemap_width, 1u, tilemap);
+        tilemap += tilemap_width;
+        y++; y %= 32;
+        SCY_REG += 8;
+        delay(25);
+    }
+    return y - 16;
+}
+
+/*  Scrolls leftward to the next tilemap.
+ *
+ *  @param x X-coordinate to start at.
+ *  @param y Y-coordinate to start at.
+ *  @param tilemap Tilemap to scroll to.
+ *  @param tilemap_width Width of tilemap.
+ *  @param tilemap_height Height of tilemap.
+ *  @returns X-coordinate ended at. 
+ */
+int scroll_left(uint8_t x, uint8_t y, const uint8_t * tilemap, uint8_t tilemap_width, uint8_t tilemap_height) {
+    int i = tilemap_width; 
+    y %= 32;
+    tilemap += (tilemap_width - 1);
+    while (i--) {
+        wait_vbl_done();
+        x--; x %= 32;
+        set_bkg_tiles_col(x, y, tilemap_height, tilemap_width, tilemap--);
+        SCX_REG -= 8;
+        delay(25);
+    }
+    return x;
+}
+
+/*  Scrolls rightward to the next tilemap.
+ *
+ *  @param x X-coordinate to start at.
+ *  @param y Y-coordinate to start at.
+ *  @param tilemap Tilemap to scroll to.
+ *  @param tilemap_width Width of tilemap.
+ *  @param tilemap_height Height of tilemap.
+ *  @returns X-coordinate ended at. 
+ */
+int scroll_right(uint8_t x, uint8_t y, const uint8_t * tilemap, uint8_t tilemap_width, uint8_t tilemap_height) {
+    int i = tilemap_width;
+    x += tilemap_width;
+    y %= 32;
+    while (i--) {
+        wait_vbl_done();
+        set_bkg_tiles_col(x, y, tilemap_height, tilemap_width, tilemap++);
+        x++; x %= 32;
+        SCX_REG += 8;
+        delay(25);
+    }
+    return x - 20;
+}
+
+void init_camera(const uint8_t * tiles, uint8_t tile_offset, uint8_t num_tiles, const uint8_t * _tilemap, uint8_t _tilemap_width, uint8_t _tilemap_height) {
+    
+}
+
 void scroll_camera(Player * player) {
     hideEnemies();
     hide_powerups();
-    /*  If the player is below the y-minPt and below the x-maxPt, then they left the room from the top*/
-    switch (player->dir) {
-    case UP:
-        for (int i = 0; i < 128; i+=2) {
-            camera.y_pos-=2;
-            wait_vbl_done();
-            set_camera();
-            if (player->y_pos - camera.y_pos < 136) {
-                scroll_sprite(0, 0, 2);
-                scroll_sprite(1, 0, 2);
-                scroll_sprite(2, 0, 2);
-                scroll_sprite(3, 0, 2); 
-            }
-            else player->y_pos-=2;
-        }
-        player->room_j--;
-        break;
-    case DOWN:
-        for (int i = 0; i < 128; i+=2) {
-            camera.y_pos+=2;
-            wait_vbl_done();
-            set_camera();
-            if (player->y_pos - 16 > camera.y_pos) {
-                scroll_sprite(0, 0, -2);
-                scroll_sprite(1, 0, -2);
-                scroll_sprite(2, 0, -2);
-                scroll_sprite(3, 0, -2); 
-            }
-            else player->y_pos+=2;
-        }
-        player->room_j++;
-        break;
-    case LEFT:
-        for (int i = 0; i < 160; i+=2) {
-            camera.x_pos-=2;
-            wait_vbl_done();
-            set_camera();
-            if (player->x_pos - camera.x_pos < 160) {
-                scroll_sprite(0, 2, 0);
-                scroll_sprite(1, 2, 0);
-                scroll_sprite(2, 2, 0);
-                scroll_sprite(3, 2, 0); 
-            }
-            else player->x_pos-=2;
-        }
-        player->room_i--;
-        break;
-    case RIGHT:
-        for (int i = 0; i < 160; i+=2) {
-            camera.x_pos+=2;
-            wait_vbl_done();
-            set_camera();
-            if (player->x_pos - 8 > camera.x_pos) {
-                scroll_sprite(0, -2, 0);
-                scroll_sprite(1, -2, 0);
-                scroll_sprite(2, -2, 0);
-                scroll_sprite(3, -2, 0); 
-            }
-            else player->x_pos+=2;
-        }
-        player->room_i++;
-        break;
+    switch(player->dir) {
+        case UP:
+            y = scroll_up(x, y, rooms[player->room_i][--player->room_j]->tilemap, 20u, 16u);
+            break;
+        case DOWN:
+            y = scroll_down(x, y, rooms[player->room_i][++player->room_j]->tilemap, 20u, 16u);
+            break;
+        case LEFT:
+            x = scroll_left(x, y, rooms[--player->room_i][player->room_j]->tilemap, 20u, 16u);
+            break;
+        case RIGHT:
+            x = scroll_right(x, y, rooms[++player->room_i][player->room_j]->tilemap, 20u, 16u);
+            break;
     }
-}
-
-/*  The meat and potatoes of the camera scrolling operation. Using
-    set_bkg_submap, this function draws smaller parts of the larger tilemap
-    infront of the camera. This will allow for the player to move around a map
-    larger than 32x32 tiles. 
-    
-    It's fragile. Don't touch it and don't call it anywhere other than scroll_camera. */
-static void set_camera() {
-    /*  Update hardware scroll position. */
-    SCX_REG = camera.x_pos;
-    SCY_REG = camera.y_pos;
-    /*  Up or down. */
-    map.y_pos = (uint8_t)(camera.y_pos >> 3u);
-    if (map.y_pos != old_map.y_pos) {
-        if (camera.y_pos < old_camera.y_pos)
-            set_bkg_based_submap(map.x_pos, map.y_pos, MIN(21u, tilemap_width - map.x_pos), 1, tilemap, tilemap_width, tile_offset);
-        else if (tilemap_height - 16u > map.y_pos)
-            set_bkg_based_submap(map.x_pos, map.y_pos + 16u, MIN(21u, tilemap_width - map.x_pos), 1, tilemap, tilemap_width, tile_offset);
-        old_map.y_pos = map.y_pos;
-    }
-    /*  Left or right? */
-    map.x_pos = (uint8_t)(camera.x_pos >> 3u);
-    if (map.x_pos != old_map.x_pos) {
-        if (camera.x_pos < old_camera.x_pos)
-            set_bkg_based_submap(map.x_pos, map.y_pos, 1, MIN(19u, tilemap_height - map.y_pos), tilemap, tilemap_width, tile_offset);
-        else if (tilemap_width - 20u > map.x_pos) 
-            set_bkg_based_submap(map.x_pos + 20u, map.y_pos, 1, MIN(19u, tilemap_height - map.y_pos), tilemap, tilemap_width, tile_offset);
-        old_map.x_pos = map.x_pos;
-    }
-    /*  Update old camera positions. */
-    old_camera.x_pos = camera.x_pos;
-    old_camera.y_pos = camera.y_pos;
 }
