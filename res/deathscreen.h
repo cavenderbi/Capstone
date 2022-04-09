5f5ae6d66dadd1a2b61cbb1f9996578f7934317e
shermanam <shermanam@appstate.edu>
2022-04-09 16:15:27 -0400

    full death screen made

--- res/deathscreen.h
+++ res/deathscreen.h
@@ -6,19 +6,20 @@
 #include <gbdk/platform.h>
 #include <gbdk/metasprites.h>
 
+#define deathscreen_TILE_ORIGIN 0
 #define deathscreen_TILE_H 8
 #define deathscreen_WIDTH 160
 #define deathscreen_HEIGHT 144
-#define deathscreen_TILE_COUNT 99
+#define deathscreen_TILE_COUNT 106
 #define deathscreen_MAP_ATTRIBUTES 0
 #define deathscreen_TILE_PALS deathscreen_tile_pals
 
 BANKREF_EXTERN(deathscreen)
 
 extern const palette_color_t deathscreen_palettes[4];
-extern const uint8_t deathscreen_tiles[1584];
+extern const uint8_t deathscreen_tiles[1696];
 
 extern const unsigned char deathscreen_map[360];
-extern const unsigned char* deathscreen_tile_pals[99];
+extern const unsigned char* deathscreen_tile_pals[106];
 
 #endif
