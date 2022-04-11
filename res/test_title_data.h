971abaaff63b29eb5a310a3b4482607e47724247
cavenderbi <cavenderbi@appstate.edu>
2022-04-11 15:34:55 -0400

    Deleted some old tileset, and make a few new ones.

--- res/test_title_data.h
+++
@@ -1,7 +0,0 @@
-#ifndef _TITLE_SCREEN_DATA_H
-#define _TITLE_SCREEN_DATA_H
-
-
-extern const char test_title_data[];
-
-#endif
--- src/title_screen.c
+++ src/title_screen.c
@@ -1,15 +1,19 @@
 #include <gb/gb.h>
 #include <gb/cgb.h>
-#include "../res/sketch_titlescreen.h"
+#include "../res/titlescreen.h"
 
 /*  Displays the tite screen and waits for the player to press start. */
 void show_title() {
     DISPLAY_OFF;
     
-    set_bkg_palette(0, 1, sketch_titlescreen_palettes);
-    set_bkg_data(0, sketch_titlescreen_TILE_COUNT, sketch_titlescreen_tiles);
-    set_bkg_tiles(0, 0, 20, 18, sketch_titlescreen_map);
+    set_bkg_palette(0, titlescreen_PALETTE_COUNT, titlescreen_palettes);
+    set_bkg_data(0, titlescreen_TILE_COUNT, titlescreen_tiles);
+    set_bkg_tiles(0, 0, 20, 18, titlescreen_map);
 
+    /* VBK_REG = 1; 
+    set_bkg_tiles(0, 0, 20, 18, titlescreen_tile_pals);
+    VBK_REG = 0; */
+    
     SHOW_BKG;
     DISPLAY_ON;
     // Wait for the user to press start. 
